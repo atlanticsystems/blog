45832102b3e1f796e7ef93ae5f46c82e6e443f79
Jose Bailo <jmbailo@gmail.com>
2017-05-16 08:41:35 +0100

    Change administration route middlewares

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ Route::group(['prefix' => 'blog'], function () {
     Route::get('/', 'Atsys\Blog\Http\Controllers\BlogController@index');
 });
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function () {
     Route::resource('posts', 'Atsys\Blog\Http\Controllers\PostsController');
     Route::resource('post_categories', 'Atsys\Blog\Http\Controllers\PostCategoriesController');
 });
